f9646f2a622b2bf4a559bc9ed127242b454202a8
Wojciech Łuczak <140091113+WojciechLuczak@users.noreply.github.com>
2023-07-26 20:48:42 +0200

    added updated files
    added updated code with functions in js files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-console.log("witajcie developerzy");
-
-let photo = document.querySelector(".headerContainer__photo");
-let przycisk = document.querySelector(".headerContainer__button");
-
-przycisk.addEventListener("click", () => {
-    if (photo.src.endsWith("img/image_1.jpg")) {
-        photo.src = "img/image_2.jpg";
-    } else if (photo.src.endsWith("img/image_2.jpg")) {
-        photo.src = "img/image_3.jpg";
-    } else if (photo.src.endsWith("img/image_3.jpg")) {
-        photo.src = "img/image_1.jpg";
+function changeImage() {
+    const photo = document.querySelector(".headerContainer__photo");
+    const currentImage = photo.src.split("/").pop();
+  
+    if (currentImage === "image_1.jpg") {
+      photo.src = "img/image_2.jpg";
+    } else if (currentImage === "image_2.jpg") {
+      photo.src = "img/image_3.jpg";
+    } else if (currentImage === "image_3.jpg") {
+      photo.src = "img/image_1.jpg";
     }
-});
+  }
+  
+  let przycisk = document.querySelector(".headerContainer__button");
+  przycisk.addEventListener("click", changeImage);
