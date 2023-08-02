2ef122bee9d2a635291148593f8a929ea02df3ba
Wojciech Łuczak <140091113+WojciechLuczak@users.noreply.github.com>
2023-08-02 20:13:19 +0200

    added updated files after corrections

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-function changeImage() {
+{
+  function changeImage() {
     const photo = document.querySelector(".headerContainer__photo");
     const currentImage = photo.src.split("/").pop();
-  
-    if (currentImage === "image_1.jpg") {
-      photo.src = "img/image_2.jpg";
-    } else if (currentImage === "image_2.jpg") {
-      photo.src = "img/image_3.jpg";
-    } else if (currentImage === "image_3.jpg") {
-      photo.src = "img/image_1.jpg";
+
+    switch (currentImage) {
+      case "image_1.jpg":
+        photo.src = "img/image_2.jpg";
+        break;
+      case "image_2.jpg":
+        photo.src = "img/image_3.jpg";
+        break;
+      case "image_3.jpg":
+        photo.src = "img/image_1.jpg";
+        break;
     }
+    /* 
+     if (currentImage === "image_1.jpg") {
+       photo.src = "img/image_2.jpg";
+     } else if (currentImage === "image_2.jpg") {
+       photo.src = "img/image_3.jpg";
+     } else if (currentImage === "image_3.jpg") {
+       photo.src = "img/image_1.jpg";
+     }*/
   }
-  
+
   let przycisk = document.querySelector(".headerContainer__button");
   przycisk.addEventListener("click", changeImage);
+}
